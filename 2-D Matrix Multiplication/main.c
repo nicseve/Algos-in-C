42094b32fbe32cfaf479a4878ca8f9a647d1f7a7
Nicholas Seve <nicseve1@gmail.com>
2017-06-11 13:56:28 -0400

    Program performs basic 2-D matrix multiplacation and prints out resulting array -NOTE: Data is hardcoded. User defined arrays will be implemented in future

---
+++ 2-D Matrix Multiplication/main.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+
+int A [3] [2] = { 
+	{1,2},
+	{3,4},
+	{5,6}
+};
+
+int B [2] [4] = {
+	{3,2,1,5},
+	{5,3,2,9}
+	};
+	
+	
+	size_t total1 = sizeof(A);  //CALCULATES TOTAL SIZES OF ARRAYS
+	size_t total2 = sizeof(B);
+	
+	
+	
+void matrixP(int r, int c, int r2, int c2, int a[r] [c], int b[r2] [c2]){
+
+	if (c != r2){  //TESTS IF THE SIZES OF THE ARRAYS ALLOW FOR MULTIPLICATION BY COMPARING #COLUMNS IN A GIVEN ARRAY TO #ROWS IN THE OTHER
+		
+		printf("Matrices cannot be multiplied. A Rows: %d, B Columns: %d",c, r2);
+	}
+	
+	else{
+		
+		int C [r] [c2]; //SIZE OF RESULTING ARRAY IS DECLARED HERE 
+		
+		for(int i = 0; i < r; i++){   //MATRIX MULTIPLYING ALGO
+			for(int j = 0; j < c2; j++){
+				for(int t=0; t < c; t ++){
+					C [i][j] += (a[i][t])*(b[t][j]);
+				}
+			}
+		}
+		
+		//PRINT ARRAY C
+		printf("C [\n");   
+		for(int u = 0; u < r; u++){
+		
+			if (u >= 1){printf("\n");
+			}
+			
+			for(int k = 0; k < c2; k++){
+				printf(" %d,",C [u][k]);
+			}
+		}
+		printf("]\n");
+	}
+
+}
+	
+	
+int main(int argc, char **argv)
+{
+	size_t column1 = sizeof(A[0])/sizeof(A[0] [0]); //CALCULATES #COLUMNS OF ARRAYS MUST BE INITALIZED AT RUN-TIME RATHER THAN COMPILE TIME
+	size_t column2 = sizeof(B[0])/sizeof(B[0] [0]); 
+	
+	size_t row1 = total1/sizeof(A[0]); //CALCULATES #ROWS OF ARRAYS MUST BE INITALIZED AT RUN-TIME RATHER THAN COMPILE TIME
+	size_t row2 = total2/sizeof(B[0]);
+	
+	matrixP(row1, column1, row2, column2, A, B);  //Multiplying function called here with int dimensions of both arrays and arrays
+	
+	
+	return 0;
+
+}
